d94c3d3fec6bb1167bcdf34acdd6579a32bcfb65
Gilles Foinet <gilles.foinet@sirehna.com>
2020-10-14 14:43:43 +0200

    export csv episode

--- eval/eval_ppo2_ShipNav4.py
+++ eval/eval_ppo2_ShipNav4.py
@@ -7,33 +7,36 @@ Created on Tue Jun  9 11:29:11 2020
 """
 
 import gym
-
+import gym_ShipNavigation
 from stable_baselines.ppo2 import PPO2
 from stable_baselines.common.evaluation import evaluate_policy
+import csv
+import numpy as np
 
-env = gym.make("ShipNav-v4")
+env = gym.make("ShipNav-v1",n_rocks=30,n_rocks_obs = 1,obs_radius = 300)
 
 
 # Load the trained agent
-model = PPO2.load("ppo2_ShipNav/final_model.zip")
+model = PPO2.load("../zoo/ppo2_ShipNav_retrain/final_model.zip")
 
 # Evaluate the agent
 #mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
 
 # Enjoy trained agent
 
-done = False
-i = 0
-reward =0
-for i in range(10):
-    obs = env.reset()
-    j = 0
-    done = False
-    reward = 0
-    while not (done or j >= 500):
-        j+=1
-        action, _states = model.predict(obs)
-        obs, rewards, done, info = env.step(action)
-        reward+=rewards
+
+    
+with open('policy_logs.csv', mode='w') as policy_logs:
+    policy_logs_writer = csv.writer(policy_logs, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    o, r, d, ep_ret, ep_len, n = env.reset(), 0, False, 0, 0, 0
+    while n < 1:
         env.render()
-    print("trial %d reward = %f"%(i,reward))
+        a, _states = model.predict(o)
+        policy_logs_writer.writerow(np.append(np.append(o,a),np.argmax(a)))
+        o, r, d, _ = env.step(a)
+        ep_ret += r
+        ep_len += 1
+    
+        if d or (ep_len == 10000):
+            o, r, d, ep_ret, ep_len = env.reset(), 0, False, 0, 0
+            n += 1
